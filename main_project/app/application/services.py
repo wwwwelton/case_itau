e5c8e786a0600681d0b0a3e78db2d6bea8256050
Welton Leite <av4t@hotmail.com>
2024-09-02 04:33:15 -0300

    refactor: remove all unused projects

--- main_project/app/application/services.py
+++
@@ -1,7 +0,0 @@
-class BookService:
-    def get_recommendations(self, gender, favorite_author):
-        return (
-            ["1984", "Animal Farm", "Homage to Catalonia"]
-            if favorite_author == "George Orwell"
-            else []
-        )
--- service1/run.py
+++
@@ -1,4 +0,0 @@
-from app.interfaces.api import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
